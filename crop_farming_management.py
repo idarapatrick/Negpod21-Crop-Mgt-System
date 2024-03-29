4da3b973eb1c51ca2ef803b82026549d4080b4d6
Idara Patrick <essienidara67@gmail.com>
2024-03-29 17:58:05 +0200

    Added first file

---
+++ crop_farming_management.py
@@ -0,0 +1,7 @@
+def introduction():
+ #Prints a welcome message and application overview
+  print("Welcome to the Crop Farming Management System!")
+  print("This application is designed to connect farmers and buyers,")
+  print("facilitating crop shipment and transactions.")
+  print("We offer various agricultural services to suit your needs.\n")
+introduction()
