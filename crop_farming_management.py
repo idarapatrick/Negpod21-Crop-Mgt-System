772170a61f84a6818cf2aeefc9e157e9805ab081
Idara Patrick <essienidara67@gmail.com>
2024-03-29 18:17:20 +0200

    added the user guides

--- crop_farming_management.py
+++ crop_farming_management.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def introduction():
  #Prints a welcome message and application overview
   print("Welcome to the Crop Farming Management System!")
@@ -6,7 +6,19 @@ def introduction():
   print("facilitating crop shipment and transactions.")
   print("We offer various agricultural services to suit your needs.\n")
 introduction()
-=======
-'Hello world!'
-print('The indispensable coding meeting session')
->>>>>>> 3ddc9ab70f44607ff2daf80cbee847b9226a30ef
+
+
+def user_guide():
+  """Prints the user guide content within the program."""
+  user_guide_text = """
+  **User Guide:**
+
+  1. To see a list of available services, type 'services'.
+  2. To connect with a buyer/seller, type 'connect' (feature under development).
+  3. To learn more about specific services, type 'info <service_name>'. (e.g., info crop_shipment)
+  4. For help, type 'help'.
+  5. To exit, type 'quit'.
+  """
+  #Print the user guide text using a multi-line string
+  print(user_guide_text)
+user_guide()
