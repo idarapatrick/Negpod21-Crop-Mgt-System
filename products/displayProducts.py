b8cefb56d95c7c79b0880e8e0b51311f9c727b75
Yassin Hagenimana <hyassin509@gmail.com>
2024-04-14 21:09:07 +0200

    display

--- products/displayProducts.py
+++ products/displayProducts.py
@@ -1,34 +1,29 @@
+# In this snippet, we connect to the MySQL database and retrieve all the products from the products table. We then use the tabulate library to display the products in a tabular format. Finally, we close the cursor and connection to the database. This snippet can be used to display the products in a user-friendly way in the console or terminal.
 import mysql.connector
 from tabulate import tabulate
 
-def display_products():
-    # Connect to the MySQL database
-    cnx = mysql.connector.connect(
+# Connect to the MySQL database
+cnx = mysql.connector.connect(
         host="localhost",
         user="root",
         password="Yassin@123",
         database="crops_mgmt"
-    )
+)
 
-    # Create a cursor object to execute SQL queries
-    cursor = cnx.cursor()
+# Create a cursor object to execute SQL queries
+cursor = cnx.cursor()
+def display_products():
+    # Execute the SQL query to retrieve the products
+    query = "SELECT * FROM products"
+    cursor.execute(query)
 
-    try:
-        # Execute the SQL query to retrieve the products
-        query = "SELECT * FROM products"
-        cursor.execute(query)
+    # Fetch all the products from the result set
+    products = cursor.fetchall()
 
-        # Fetch all the products from the result set
-        products = cursor.fetchall()
+    # Display the products in a tabular format
+    print(tabulate(products, headers=['ID', 'Name', 'Price', 'Location', 'Farmer', 'Quantity','Expiration Date'], tablefmt='pretty'))
 
-        # Display the products in a tabular format
-        print(tabulate(products, headers=['ID', 'Name', 'Price', 'Location', 'Farmer', 'Quantity','Expiration Date'], tablefmt='pretty'))
-    except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        cnx.close()
-
-# Call the function to display the products
-display_products()
+    # Close the cursor and connection
+    cursor.close()
+    cnx.close()
+# display_products()
