4062e07f814e4466fa7c2ace374fa814ed93b9f1
DUSHIME Dan Paul <danpauldushime03@gmail.com>
2024-04-16 12:20:09 -0400

    Made some changes to the orderingproduct.py

--- products/orderingproduct.py
+++ products/orderingproduct.py
@@ -30,8 +30,8 @@ class Order:
 cnx = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Yassin@123",
-    database="crops_mgmt"
+    password="1234",
+    database="Crop-Mgt-System"  # Removed leading space
 )
 
 # Create a cursor object
@@ -40,8 +40,40 @@ cursor = cnx.cursor()
 # Create an order
 order = Order()
 
-# Ask the user what they wish to purchase and their location
-print("What do you wish to purchase?")
+print("What do you wish to purchase? These are the available products with the first number being the product id:")
+print("2. BANANA")
+print("4. Pineapple")
+print("9. Oranges")
+print("6. Watermelon")
+print("23. Milk")
+print("3. Mangoes")
+print("Enter a Product: ", end="")
+product_choice = int(input())
+if product_choice == 1:
+    print("The cost of One BANANA IS 5$")
+elif product_choice == 2:
+    print("THE COST OF ONE PINEAPPLE IS 3000$")
+elif product_choice == 3:
+    print("The cost of one orange is 12$")
+elif product_choice == 4:
+    print("The cost of watermelon is 1000$")
+elif product_choice == 5:
+    print("The cost of one liter of Milk is 12$")
+elif product_choice == 6:
+    print("The cost of Mango is 3000$")
+else:
+    print("Invalid choice. Please enter a number between 1 and 6.")
+
+answer = input("Do you wish to continue with the purchase? (yes/no): ")
+if answer.lower() == 'no':
+    print("Okay, no problem. Have a great day!")
+    exit(0)  # This will stop the execution of the script
+elif answer.lower() == 'yes':
+    # Corrected the print statement
+    print("Great! Let's continue with the purchase.")
+else:
+    print("Invalid response. Please answer with 'yes' or 'no'.")
+
 print("Where is your location? Please choose from the following provinces of Rwanda:")
 print("1. Southern")
 print("2. Northern")
@@ -63,10 +95,17 @@ elif location == '5':
     print("A drone will deliver the product")
 
 # Add products to the order
+product_count = 0  # Counter to track the number of products added
 while True:
-    add_more = input("Do you want to add a product to the order? (yes/no): ")
-    if add_more.lower() == 'yes':
-        product_id = input("Enter the ID of the product to update: ")
+    if product_count == 0:
+        add_more = input("Do you want to add a product to the order? (yes/no): ")
+    else:
+        add_more = input("Do you want to add another product to the order? (yes/no): ")
+
+    if add_more.lower() not in ['yes', 'no']:
+        raise ValueError("Invalid response. Please answer with 'yes' or 'no'.")
+    elif add_more.lower() == 'yes': 
+        product_id = input("Enter the number your preferred product ID : ")
 
         # Now you can execute SQL queries
         query = ("SELECT * FROM products WHERE id = %s")
@@ -82,11 +121,14 @@ while True:
 
         # Add the product to the order
         order.add_product(product, quantity)
+
+        product_count += 1  # Increment the counter after a product is added
     else:
         break
 
+
 # Print the total price of the order
-print("Total price of the order is:", order.total_price())
+print("Total price of the order is:", order.total_price())  # Corrected the method call
 
 # Don't forget to close the cursor and connection
 cursor.close()
